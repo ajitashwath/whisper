a416b91eba7d0d3107f6271aaaf05882ded5f592
ajitashwathr10 <ajitashwathr23@lpu.in>
2025-05-18 13:51:45 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import './globals.css';
-import type {Metadata} from 'next';
-import {Inter} from 'next/font/google';
-import {ThemeProvider} from '@/components/theme-provider';
-import {Toaster} from "@/components/ui/toaster";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Whisper | Self-Destructing Secret Messages',
@@ -17,11 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang = "en" suppressHydrationWarning>
-      <body className = {inter.className}>
-        <ThemeProvider attribute = "class" defaultTheme = "dark" enableSystem>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
-          <Toaster/>
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
